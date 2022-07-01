d409bd37c4944aab02a6195c6f4750918a331703
Ammar Khazal <ammar@vicompany.nl>
2022-07-01 16:42:54 +0200

    Orders page looks good for now

--- services/ordersService.js
+++ services/ordersService.js
@@ -24,8 +24,10 @@ function _convertOrdersToOrderViewModels(orders) {
         const viewModel = new OrderViewModel();
         const orderLineViewModels = _convertOrderLinesToOrderLineViewModels(o.orderLines);
 
+        viewModel.id = o.id;
         viewModel.orderLines = orderLineViewModels;
         viewModel.sumTotal = _sumOrderLineViewModelsTotalPrices(orderLineViewModels);
+        viewModel.date =  _getFormattedDate(o.createdAt);
 
         orderViewModels.push(viewModel);
     })
@@ -57,4 +59,9 @@ function _sumOrderLineViewModelsTotalPrices(cartLineViewModelsArray) {
     return cartLineViewModelsArray
         .reduce(((x, { totalPrice }) => x + totalPrice), 0.0)
         .toFixed(2);
+}
+
+function _getFormattedDate(date) {
+    return ("0" + date.getDate()).slice(-2) + "-" + ("0"+(date.getMonth()+1)).slice(-2) + "-" +
+    date.getFullYear() + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2);
 }
